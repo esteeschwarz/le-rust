07cc9c1ece0f4534736525f092a42e5b84c51f14
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-03-23 18:25:29 +0100

    "

--- essais/ids-log/src/main.rs
+++ essais/ids-log/src/main.rs
@@ -155,7 +155,7 @@ fn init_db(conn: &Connection) -> rusqlite::Result<()> {
     }
     fn check_create_pwd(conn: &Connection, table_name: &str, password: &str,masterpassword:&str) -> rusqlite::Result<bool> {
         let mut stmt = conn.prepare("SELECT field2 FROM mastertable WHERE field1 = ?1")?;
-        let mut rows = stmt.query(params!['createtablepassword'])?;
+        let mut rows = stmt.query(params!["createtablepassword"])?;
     
         if let Some(row) = rows.next()? {
             let stored_password: String = row.get(0)?;
