184fffd88b9614c74961b7753eb1fb823fbbefaf
estee <91368979+esteeschwarz@users.noreply.github.com>
2025-03-28 16:58:45 +0100

    002crt

--- essais/ids-log/src/main.rs
+++ essais/ids-log/src/main.rs
@@ -171,7 +171,7 @@ fn init_db_dep(conn: &Connection) -> rusqlite::Result<()> {
         let mut rows = stmt.query(params!["createtablepassword"])?;
         let mut stored_password: String = generate_random_string().to_string();
         println!("create pwd stored before fetch db {}",stored_password);
-        stored_password = row.get(0)?;
+        stored_password = rows.get(0)?;
         println!("create pwd in fetch db {}",stored_password);
         println!("master pwd provided {}",masterpassword);
       
